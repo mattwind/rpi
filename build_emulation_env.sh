de3beea6219baca7328cc73c1429c9b4e5750c87
Matt Wind <mattwind@gmail.com>
2018-10-29 14:48:57 -0700

    notes

--- build_emulation_env.sh
+++ build_emulation_env.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+
 WORKING_DIR=`pwd`
 KERNEL_URL="https://raw.githubusercontent.com/dhruvvyas90/qemu-rpi-kernel/master/kernel-qemu-4.4.34-jessie"
 RASPBIAN_LITE_URL="http://director.downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2018-10-11/2018-10-09-raspbian-stretch-lite.zip"
 RESIZE_AMMOUNT="6G"
+
+# Tools
+sudo -y apt-get install qemu-system zip unzip
 
 # Kernel
 mkdir -p "$WORKING_DIR/kernel"
@@ -26,3 +30,6 @@ rm -rf "$WORKING_DIR/vm/*"
 mkdir -p "$WORKING_DIR/vm"
 qemu-img convert -f raw -O qcow2 "$WORKING_DIR/raspbian/2018-10-09-raspbian-stretch-lite.img" "$WORKING_DIR/vm/raspbian.qcow"
 qemu-img resize "$WORKING_DIR/vm/raspbian.qcow" +$RESIZE_AMOUNT
+
+echo
+echo "Finished setup. Try it out, bash run_rpi.sh"
