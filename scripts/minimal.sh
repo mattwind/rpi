1811d66d1e93874945b2102b92f905558d81d79e
Matt Wind <mattwind@gmail.com>
2018-10-30 16:00:04 -0700

    minimal x

--- scripts/minimal.sh
+++ scripts/minimal.sh
@@ -2,4 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install -y xorg fluxbox slim thunar network-manager-gnome
+sudo sed -i 's/#auto_login          no/auto_login          yes/' /etc/slim.conf
+sudo sed -i 's/#default_user        simone/default_user        pi/' /etc/slim.conf
 sudo systemctl start slim
