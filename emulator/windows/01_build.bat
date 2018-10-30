2994b7fc4801f749971cf46c48faa18ca9a1656f
Matt Wind <mattwind@gmail.com>
2018-10-30 09:24:22 -0700

    fix vm path

--- emulator/windows/01_build.bat
+++ emulator/windows/01_build.bat
@@ -2,14 +2,14 @@
 
 SET RESIZE=6G
 
+mkdir vm
+
 if exist "raspbian\2018-10-09-raspbian-stretch-lite.img" (
   echo Converting...
-  qemu-img convert -f raw -O qcow2 "raspbian/2018-10-09-raspbian-stretch-lite.img" "raspbian.qcow"
-  qemu-img resize "raspbian.qcow" +%RESIZE%
+  qemu-img convert -f raw -O qcow2 "raspbian/2018-10-09-raspbian-stretch-lite.img" "vm\raspbian.qcow"
+  qemu-img resize "vm\raspbian.qcow" +%RESIZE%
 ) else (  
   type "raspbian\README.md"
   explorer "raspbian"
   pause
 )
-
-
