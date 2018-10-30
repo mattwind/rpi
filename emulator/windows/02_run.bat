c31f7412ae451131c8398fe8312c62c6bcc3ef1f
Matt Wind <mattwind@gmail.com>
2018-10-30 13:24:32 -0700

    run opts

--- emulator/windows/02_run.bat
+++ emulator/windows/02_run.bat
@@ -1,4 +1,7 @@
 SET KERNEL=kernel/kernel-qemu-4.4.34-jessie
 SET VM=vm/raspbian.qcow
+SET MEM="256"
+SET CPU="arm1176"
+SET MODEL="versatilepb"
 
-qemu-system-arm -kernel %KERNEL% -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -hda %VM% -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -net nic -net user,hostfwd=tcp::10022-:22
+qemu-system-arm -kernel %KERNEL% -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -hda %VM% -cpu %CPU% -m %MEM% -M %MODEL% -display sdl -no-reboot -serial stdio -net nic -net user,hostfwd=tcp::10022-:22
