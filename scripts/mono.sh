5b52908d8c1514e673e44b43cbeac17de62842a0
Matt Wind <mattwind@gmail.com>
2018-11-01 11:40:06 -0700

    typo

--- scripts/mono.sh
+++ scripts/mono.sh
@@ -8,7 +8,7 @@ VERSION="5.14.0.177-0xamarin3+raspbian9b1"
 
 sudo apt install apt-transport-https dirmngr
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $KEY
-echo "deb $URL/$OS $SNAPSHOT main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+echo "deb $REPO/$OS $SNAPSHOT main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
 sudo apt-get install mono-complete=$VERSION
 
