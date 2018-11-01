2d619d9e652087d5f528da8721f3e4226b7bbcf8
Matt Wind <mattwind@gmail.com>
2018-11-01 11:37:26 -0700

    update mono repo

--- scripts/mono.sh
+++ scripts/mono.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+REPO="https://download.mono-project.com/repo"
+OS="debian"
 SNAPSHOT="raspbianstretch/snapshots/5.14.0.177"
+KEY="3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+VERSION="5.14.0.177-0xamarin3+raspbian9b1"
 
 sudo apt install apt-transport-https dirmngr
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/debian $SNAPSHOT main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $KEY
+echo "deb $URL/$OS $SNAPSHOT main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-sudo apt-get install mono-complete=5.14.0.177-0xamarin3+raspbian9b1
+sudo apt-get install mono-complete=$VERSION
+
